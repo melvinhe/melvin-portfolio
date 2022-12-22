edd34a5b0778e313229fdc093f357c2d2a0b3258
melvinhe <melvinhe2014@gmail.com>
2022-12-22 09:48:41 -0500

    feat: icons, tailwend, document update

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,55 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx)
+    return { ...initialProps }
+  }
+
+  render() {
+    return (
+      // open graph protocol
+      <Html prefix="og: http://ogp.me/ns#">
+        <Head>
+          {/*Favicons*/}
+          <link rel="apple-touch-icon" href="/apple-touch-icon.png" sizes="180x180" />
+          <link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32"/>
+          <link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16"/>
+          <link rel="manifest" href="/manifest.json"/>
+
+          {/*inter web font*/}
+          <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
+          {/*font awesome icons*/}
+          <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossOrigin="anonymous" />
+        
+          {/*metadata*/}
+          <meta
+            property="og:title"
+            content="Melvin's Portfolio"
+          />
+          <meta
+            name="image"
+            property="og:image"
+            content="/preview_thumbnail.png"
+          />
+          <meta
+            name="author"
+            content="Melvin He"
+          />
+          <meta
+            property="og:description"
+            content="Welcome to Melvin He's website! Here you can find out more about me and what I've been up to."
+          />
+          <meta
+            property="og:url"
+            content="https://melvin-he.dev"/>
+      </Head>
       <body>
-        <Main />
-        <NextScript />
+      <Main/>
+      <NextScript/>
       </body>
-    </Html>
-  )
+      </Html>
+    )
+  }
 }
+

980fabbf890d98bc29420562f6aa46d9fed9aa76
Melvin He <melvin_he@brown.edu>
2023-01-04 16:42:23 -0500

    document update

--- pages/_document.js
+++ pages/_document.js
@@ -12,9 +12,10 @@ export default class MyDocument extends Document {
       <Html prefix="og: http://ogp.me/ns#">
         <Head>
           {/*Favicons*/}
-          <link rel="apple-touch-icon" href="/apple-touch-icon.png" sizes="180x180" />
-          <link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32"/>
-          <link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16"/>
+
+          <link rel="apple-touch-icon" href="/favicon.png" sizes="180x180" />
+          <link rel="icon" type="image/png" href="/favicon.png" sizes="32x32"/>
+          <link rel="icon" type="image/png" href="/favicon.png" sizes="16x16"/>
           <link rel="manifest" href="/manifest.json"/>
 
           {/*inter web font*/}
